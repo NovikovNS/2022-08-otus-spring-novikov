9661651e736b7657be93624ac8551dc9e0eb395d
Nikita.Novikov <nikita.novikov@rt.ru>
2022-11-06 18:45:27 +0300

    [homework-8] Added tests

--- library_mongo/src/main/java/ru/otus/homework8/mongock/changelog/LibraryChangelog.java
+++ library_mongo/src/main/java/ru/otus/homework8/mongock/changelog/LibraryChangelog.java
@@ -32,35 +32,41 @@ public class LibraryChangelog {
             Comment.builder().comment("Классная книга").build()
     );
 
-
     @ChangeSet(order = "000", id = "dropDB", author = "novikov", runAlways = true)
     public void dropDB(MongoDatabase database){
         database.drop();
     }
 
-    @ChangeSet(order = "001", id = "initAuthors", author = "novikov", runAlways = true)
+    @ChangeSet(order = "001", id = "initAuthors", author = "novikov")
     public void initAuthors(AuthorRepository authorRepository) {
         authorRepository.save(authors.get(0));
+        authorRepository.save(authors.get(1));
+        authorRepository.save(authors.get(2));
     }
 
-    @ChangeSet(order = "002", id = "initStyles", author = "novikov", runAlways = true)
+    @ChangeSet(order = "002", id = "initStyles", author = "novikov")
     public void initStyles(StyleRepository styleRepository) {
         styleRepository.save(styles.get(0));
+        styleRepository.save(styles.get(1));
+        styleRepository.save(styles.get(2));
     }
 
 
-    @ChangeSet(order = "003", id = "initComments", author = "novikov", runAlways = true)
+    @ChangeSet(order = "003", id = "initComments", author = "novikov")
     public void initComments(CommentRepository commentRepository) {
         commentRepository.save(comments.get(0));
+        commentRepository.save(comments.get(1));
+        commentRepository.save(comments.get(2));
     }
 
-    @ChangeSet(order = "004", id = "initBooks", author = "novikov", runAlways = true)
+    @ChangeSet(order = "004", id = "initBooks", author = "novikov")
     public void initBook(BookRepository bookRepository) {
-        Book book = bookRepository.save(Book.builder()
-                .name("Проверка")
-                .author(authors.get(0))
-                .style(styles.get(0))
-                .comments(List.of(comments.get(0))).build());
+        bookRepository.save(Book.builder().name("Евгений Онегин").author(authors.get(0))
+                .style(styles.get(0)).comments(List.of(comments.get(0))).build());
+        bookRepository.save(Book.builder().name("Шерлок Холмс").author(authors.get(1))
+                .style(styles.get(1)).comments(List.of(comments.get(1))).build());
+        bookRepository.save(Book.builder().name("Сияние").author(authors.get(2))
+                .style(styles.get(2)).comments(List.of(comments.get(2))).build());
     }
 
 }
---
+++ library_mongo/src/test/java/ru/otus/homework8/dao/BookRepositoryTest.java
@@ -0,0 +1,52 @@
+package ru.otus.homework8.dao;
+
+import com.github.cloudyrock.spring.v5.EnableMongock;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import ru.otus.homework8.domain.Book;
+import ru.otus.homework8.mongock.changelog.TestLibraryChangelog;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataMongoTest
+@EnableMongock
+public class BookRepositoryTest {
+
+    private static final long INITIAL_LIBRARY_SIZE = 2L;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    void shouldFindBookById() {
+        Query query = new Query().addCriteria(Criteria.where("id").is(TestLibraryChangelog.BOOK_1.getId()));
+        Book expectedBook = mongoTemplate.findOne(query, Book.class);
+        assertThat(expectedBook).usingRecursiveComparison().isEqualTo(TestLibraryChangelog.BOOK_1);
+    }
+
+    @Test
+    void shouldFindAllBooks() {
+        assertEquals(INITIAL_LIBRARY_SIZE, bookRepository.findAll().size());
+    }
+
+    @Test
+    void shouldDeleteCommentFromBook() {
+        val commentsBeforeDeleting = bookRepository.findBookById(TestLibraryChangelog.BOOK_1.getId()).get()
+                .getComments();
+        assertEquals(1, commentsBeforeDeleting.size());
+        bookRepository.deleteCommentFromBookByCommentId(TestLibraryChangelog.COMMENTS.get(0).getId());
+        val commentsAfterDeleting = bookRepository.findBookById(TestLibraryChangelog.BOOK_1.getId()).get()
+                .getComments();
+        assertThat(commentsAfterDeleting).isEmpty();
+    }
+
+}
